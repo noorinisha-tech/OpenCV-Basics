5d32d1655e222f69486c1ebfe6814142bbcf1d2c
noorinisha-tech <noorisanah.1927@gmail.com>
2026-08-07 18:29:37 +0530

    Day 18 - Adaptive Thresholding

---
+++ day18_adaptive_thresholding.py
@@ -0,0 +1,36 @@
+import cv2
+
+# Read image
+img = cv2.imread("shapes.png")
+
+# Convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Adaptive Mean Threshold
+adaptive_mean = cv2.adaptiveThreshold(
+    gray,
+    255,
+    cv2.ADAPTIVE_THRESH_MEAN_C,
+    cv2.THRESH_BINARY,
+    11,
+    2
+)
+
+# Adaptive Gaussian Threshold
+adaptive_gaussian = cv2.adaptiveThreshold(
+    gray,
+    255,
+    cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    cv2.THRESH_BINARY,
+    11,
+    2
+)
+
+# Display
+cv2.imshow("Original", img)
+cv2.imshow("Grayscale", gray)
+cv2.imshow("Adaptive Mean", adaptive_mean)
+cv2.imshow("Adaptive Gaussian", adaptive_gaussian)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
